9ae74323748df8ca0b210906845eee88a22b7e8d
DESKTOP-LSLTHMA\Horlescu <adrian.horlescu@gmail.com>
2024-12-22 20:45:15 +0200

    setting up pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import { Bungalows } from "./pages/Bungalows";
+import { Bookings } from "./pages/Bookings";
+import { NewUsers } from "./pages/Users";
+import { Settings } from "./pages/Settings";
+import { Account } from "./pages/Account";
+import { Login } from "./pages/Login";
+import { PageNotFound } from "./pages/PageNotFound";
 
 export function App() {
-    return <div>Hello</div>;
+    return (
+        <>
+            <GlobalStyles />
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        index
+                        element={<Navigate replace to="dashboard" />}
+                    />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="bookings" element={<Bookings />} />
+                    <Route path="bungalows" element={<Bungalows />} />
+                    <Route path="users" element={<NewUsers />} />
+                    <Route path="settings" element={<Settings />} />
+                    <Route path="account" element={<Account />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
---
+++ src/pages/Bungalows.jsx
@@ -0,0 +1,11 @@
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+
+export function Bungalows() {
+    return (
+        <Row type="horizontal">
+            <Heading as="h1">All Bungalows </Heading>
+            <p>TEST</p>
+        </Row>
+    );
+}
