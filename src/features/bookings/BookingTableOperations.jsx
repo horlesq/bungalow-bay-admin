1a4523d1acfec4a8c17a69c92a4684b2a492dc42
DESKTOP-LSLTHMA\Horlescu <adrian.horlescu@gmail.com>
2025-01-29 18:46:22 +0200

    server side filtering bookings

--- src/features/bookings/BookingTableOperations.jsx
+++ src/features/bookings/BookingTableOperations.jsx
@@ -1,33 +1,40 @@
-import SortBy from "../../ui/SortBy";
-import Filter from "../../ui/Filter";
-import TableOperations from "../../ui/TableOperations";
+import { Sort } from "../../ui/Sort";
+import { Filter } from "../../ui/Filter";
+import { TableOperations } from "../../ui/TableOperations";
 
-function BookingTableOperations() {
-  return (
-    <TableOperations>
-      <Filter
-        filterField="status"
-        options={[
-          { value: "all", label: "All" },
-          { value: "checked-out", label: "Checked out" },
-          { value: "checked-in", label: "Checked in" },
-          { value: "unconfirmed", label: "Unconfirmed" },
-        ]}
-      />
+export function BookingTableOperations() {
+    return (
+        <TableOperations>
+            <Filter
+                filterField="status"
+                options={[
+                    { value: "all", label: "All" },
+                    { value: "checked-out", label: "Checked out" },
+                    { value: "checked-in", label: "Checked in" },
+                    { value: "unconfirmed", label: "Unconfirmed" },
+                ]}
+            />
 
-      <SortBy
-        options={[
-          { value: "startDate-desc", label: "Sort by date (recent first)" },
-          { value: "startDate-asc", label: "Sort by date (earlier first)" },
-          {
-            value: "totalPrice-desc",
-            label: "Sort by amount (high first)",
-          },
-          { value: "totalPrice-asc", label: "Sort by amount (low first)" },
-        ]}
-      />
-    </TableOperations>
-  );
+            <Sort
+                options={[
+                    {
+                        value: "startDate-desc",
+                        label: "Date: recent first",
+                    },
+                    {
+                        value: "startDate-asc",
+                        label: "Date: earlier first",
+                    },
+                    {
+                        value: "totalPrice-desc",
+                        label: "Amount: high to low",
+                    },
+                    {
+                        value: "totalPrice-asc",
+                        label: "Amount: low to high",
+                    },
+                ]}
+            />
+        </TableOperations>
+    );
 }
-
-export default BookingTableOperations;
--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,12 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+    const [searchParams] = useSearchParams();
+
+    // Get the filter from the URL
+    const currentFilter = searchParams.get("filter");
+    const filter =
+        !currentFilter || currentFilter === "all"
+            ? null
+            : { field: "status", value: currentFilter };
+
     const {
         isLoading,
         data: bookings,
         error,
-    } = useQuery({ queryKey: ["bookings"], queryFn: getBookings });
+    } = useQuery({
+        queryKey: ["bookings", filter],
+        queryFn: () => getBookings({ filter }),
+    });
 
     return { isLoading, bookings, error };
 }
