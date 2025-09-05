2075409075584b96092ac1a6141177c254484efe
horlesq <48680025+horlesq@users.noreply.github.com>
2025-09-05 07:14:55 +0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://kkmifsritqjabklifonk.supabase.co";
+// Public KEY - Row Level Security is activated for all requests
 const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrbWlmc3JpdHFqYWJrbGlmb25rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5NzI1NTUsImV4cCI6MjA1MDU0ODU1NX0.C6JI8soxvwANXhhqGSernpkjGdUhRWt8Y5fYprgm-JU`;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
